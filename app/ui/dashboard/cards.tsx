5214d4a6950d26ce7c0910e300536c5fa04a9517
paula moheb <poulamoheb12@gmail.com>
2026-09-09 16:47:04 +0300

    Refactor dashboard components: restore CardWrapper, RevenueChart, and LatestInvoices; implement loading states with skeletons

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -13,21 +14,24 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-// export default async function CardWrapper() {
-//   return (
-//     <>
+export default async function CardWrapper() {
 
-//       <Card title="Collected" value={totalPaidInvoices} type="collected" />
-//       <Card title="Pending" value={totalPendingInvoices} type="pending" />
-//       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-//       <Card
-//         title="Total Customers"
-//         value={numberOfCustomers}
-//         type="customers"
-//       />
-//     </>
-//   );
-// }
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
+
+  return (
+    <>
+
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
+      />
+    </>
+  );
+}
 
 export function Card({
   title,
