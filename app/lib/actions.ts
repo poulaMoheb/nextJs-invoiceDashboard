45ece1a33e1058176cc8a5c669bc1af7621d3015
paula moheb <poulamoheb12@gmail.com>
2026-09-10 12:06:53 +0300

    Add error handling and not found page for invoice management; implement error component and 404 page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -19,18 +19,28 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 // Creating a new record in the invoices table
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: Number(formData.get('amount')),
-        status: formData.get('status'),
-    });
+    try {
 
-    const amountInCents = Math.round(amount * 100);
-    const date = new Date().toISOString().split('T')[0];
+        const { customerId, amount, status } = CreateInvoice.parse({
+            customerId: formData.get('customerId'),
+            amount: Number(formData.get('amount')),
+            status: formData.get('status'),
+        });
 
-    await sql`INSERT INTO invoices (customer_id, amount, status, date)
+        const amountInCents = Math.round(amount * 100);
+        const date = new Date().toISOString().split('T')[0];
+
+        await sql`INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
 
+    }
+    catch (error) {
+        console.error('Error: ', error);
+        return {
+            message: 'Database Error: Failed to Create Invoice.'
+        };
+
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
@@ -43,18 +53,35 @@ export async function updateInvoice(id: string, formData: FormData) {
         amount: Number(formData.get('amount')),
         status: formData.get('status'),
     });
-    const amountInCents = Math.round(amount * 100);
+    try {
+        const amountInCents = Math.round(amount * 100);
 
-    await sql`
-    UPDAte invoices SET customer_id = ${customerId}, amount=${amountInCents}, status=${status}
-    WHERE id = ${id} 
-    `
+        await sql`
+        UPDAte invoices SET customer_id = ${customerId}, amount=${amountInCents}, status=${status}
+        WHERE id = ${id} 
+        `
+    }
+    catch (error) {
+        console.error('Error: ', error);
+        return {
+            message: 'Database Error: Failed to Create Invoice.'
+        };
+
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 // Delete a record in invoice table 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+    }
+    catch (error) {
+        console.error('Error: ', error);
+        return {
+            message: 'Database Error: Failed to Create Invoice.'
+        };
+    }
     revalidatePath('dashboard/invoices');
 }
